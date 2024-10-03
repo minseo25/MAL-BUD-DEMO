fcd29a073804bf56b012523771c6a5316d85f5ee
Min seo Kim <minseokim@Minui-MacBookAir.local>
2024-10-03 23:46:35 +0900

    fix: delete unnecessary dependencies, add CORS allowed origins, change site name

--- mal-bud-backend/malbud/settings.py
+++ mal-bud-backend/malbud/settings.py
@@ -145,6 +145,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost:\d+$",
+    r"^http://127.0.0.1:\d+$",
+]
+
 CORS_ALLOWED_HEADERS = (
     "accept",
     "authorization",
